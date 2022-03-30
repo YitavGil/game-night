1193bf5984875058d781895f25e4147fbbda2560
YitavGil <yitavga@yahoo.com>
2022-03-30 12:44:23 +0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, {useEffect} from "react";
-import {useDispatch} from 'react-redux';
-import {loadGames} from './actions/gamesAction';
+import Home from "./pages/Home";
+
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadGames())
-  })
-
+  
   return (
     <div className="App">
-      <h1>Game app</h1>
+      
+      <Home />
     </div>
   );
 }
